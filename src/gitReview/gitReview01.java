1f20ac850fe89e8b0c77bde0a3b4f85a303e3ead
jenellecardona <jnel.cardona@gmail.com>
2022-05-31 21:35:38 -0400

    1st commit

---
+++ src/gitReview/gitReview01.java
@@ -0,0 +1,10 @@
+package gitReview;
+
+public class gitReview01 {
+	public static void main(String[] args) {
+		System.out.println("today we learned github");
+		System.out.println("today is a very good day");
+		
+	}
+
+}
