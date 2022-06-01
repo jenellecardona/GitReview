bcfc08f8be62efae781f5f703fcd128f9afbef73
jenellecardona <jnel.cardona@gmail.com>
2022-05-31 21:52:51 -0400

    2nd commit

--- src/gitReview/gitReview01.java
+++ src/gitReview/gitReview01.java
@@ -4,6 +4,7 @@ public class gitReview01 {
 	public static void main(String[] args) {
 		System.out.println("today we learned github");
 		System.out.println("today is a very good day");
+		System.out.println("today is a very bad day");
 		
 	}
 
